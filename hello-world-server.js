8e981991aea4a4c9dde2d4e3c4f34390440563c8
Ybarra <morfeusweb@hotmail.com>
2016-01-17 11:16:20 +0100

    Versioning

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Buenos días Larpeira 2 !!\nDesde el nuevo entorno de desarrollo recien instalado!!d\n');
+    res.end('Buenos días Larpeira 3 !!\nDesde el nuevo entorno de desarrollo recien instalado!!d\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
